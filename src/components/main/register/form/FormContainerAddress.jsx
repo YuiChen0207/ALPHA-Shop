bbe78f623519c98d0f98ac6485930adeaff233cd
YuiChen0207 <asd0935252359@gmail.com>
2023-05-06 23:23:16 +0800

    add RDW to the step 1 form

--- src/components/main/register/form/FormContainerAddress.jsx
+++ src/components/main/register/form/FormContainerAddress.jsx
@@ -1,18 +1,17 @@
+import FormItem from "./FormItem";
+import cityList from "../form/data/cityList.js";
 import styles from "./FormContainerAddress.module.scss";
-
 
 const FormContainerAddress = () => {
   return (
-    <form className={`col col-12 ${styles.form}`} data-phase="address">
+    <form className="col col-12" data-phase="address">
       <h3 className={styles.formTitle}>寄送地址</h3>
-      <section className={`form-body col col-12 ${styles.formBody}`}>
-        <div
-          className={`${styles.inputGroupContainer} ${styles.inputGroupContainerInline}`}
-        >
-          <div className={`${styles.inputGroup}`}>
-            <div className={styles.inputLabel}>稱謂</div>
+      <section className="form-body col col-12">
+        <div className={`${styles.row} col col-12`}>
+          <div className={`${styles.selectGroup} lg-2 sm-s1`}>
+            <div className={styles.selectLabel}>稱謂</div>
             <div className={styles.selectContainer}>
-              <select className={`${styles.input}`}>
+              <select>
                 <option value="mr" defaultValue>
                   先生
                 </option>
@@ -21,87 +20,53 @@ const FormContainerAddress = () => {
               </select>
             </div>
           </div>
-          <div className={`${styles.inputGroup}`}>
-            <div className={styles.inputLabel}>姓名</div>
-            <input
-              className={`${styles.input}`}
-              type="text"
-              placeholder="請輸入姓名"
-            />
-          </div>
+          <FormItem
+            selectLabel="姓名"
+            type="input"
+            placeholder="請輸入姓名"
+            classList="lg-4 sm-s1"
+          />
         </div>
-        <div className={styles.inputGroupContainerResize}>
-          <div className={`${styles.inputGroup}`}>
-            <div className={styles.inputLabel}>電話</div>
-            <input
-              className={styles.input}
-              type="tel"
-              placeholder="請輸入行動電話"
-            />
-          </div>
-          <div className={`${styles.inputGroup}`}>
-            <div className={styles.inputLabel}>Email</div>
-            <input
-              className={styles.input}
-              type="email"
-              placeholder="請輸入電子郵件"
-            />
-          </div>
+        <div className={`${styles.row} col col-12`}>
+          <FormItem
+            selectLabel="電話"
+            type="tel"
+            placeholder="請輸入行動電話"
+            classList="lg-3 sm-full"
+          />
+          <FormItem
+            selectLabel="Email"
+            type="Email"
+            placeholder="請輸入電子郵件"
+            classList="lg-3 sm-full"
+          />
         </div>
-        <div className={styles.inputGroupContainer}>
-          <div
-            className={`input-group input-w-lg-2 input-w-sm-full ${styles.inputGroup}`}
-          >
-            <div className={styles.inputLabel}>縣市</div>
+        <div className={`${styles.row} col col-12`}>
+          <div className={`${styles.selectGroup} lg-2 sm-full`}>
+            <div className={styles.selectLabel}>縣市</div>
             <div className={styles.selectContainer}>
-              <select className={styles.input} required>
-                <option value="" defaultValue disabled>
+              <select required>
+                <option value="" defaultValue>
                   請選擇縣市
                 </option>
-                <option value="KLU">基隆市</option>
-                <option value="TPH">新北市</option>
-                <option value="TPE">臺北市</option>
-                <option value="TYC">桃園市</option>
-                <option value="HSH">新竹縣</option>
-                <option value="HSC">新竹市</option>
-                <option value="MAC">苗栗市</option>
-                <option value="MAL">苗栗縣</option>
-                <option value="TXG">臺中市</option>
-                <option value="CWH">彰化縣</option>
-                <option value="CWS">彰化市</option>
-                <option value="NTC">南投市</option>
-                <option value="NTO">南投縣</option>
-                <option value="YLH">雲林縣</option>
-                <option value="CHY">嘉義縣</option>
-                <option value="CYI">嘉義市</option>
-                <option value="TNN">臺南市</option>
-                <option value="KHH">高雄市</option>
-                <option value="IUH">屏東縣</option>
-                <option value="PTS">屏東市</option>
-                <option value="ILN">宜蘭縣</option>
-                <option value="ILC">宜蘭市</option>
-                <option value="HWA">花蓮縣</option>
-                <option value="HWC">花蓮市</option>
-                <option value="TTC">臺東市</option>
-                <option value="TTT">臺東縣</option>
-                <option value="PEH">澎湖縣</option>
-                <option value="KMN">金門縣</option>
-                <option value="LNN">連江縣</option>
+                {cityList.map(({ id, city }) => (
+                  <option key={id} value={city}>
+                    {city}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
-          <div className={`${styles.inputGroup}`}>
-            <div className={styles.inputLabel}>地址</div>
-            <input
-              className={styles.input}
-              type="text"
-              placeholder="請輸入地址"
-            />
-          </div>
+          <FormItem
+            selectLabel="地址"
+            type="text"
+            placeholder="請輸入地址"
+            classList="lg-4 sm-full"
+          />
         </div>
       </section>
     </form>
   );
-}
+};
 
 export default FormContainerAddress;
